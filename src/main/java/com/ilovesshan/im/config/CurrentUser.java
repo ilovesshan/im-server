d1e740bddf79b59ae78120f2c2af6a5453475253
ilovesshan <2665939276@qq.com>
2023-05-18 16:43:24 +0800

    feat: 添加HandlerMethodArgumentResolver 获取登录当前用户信息

---
+++ src/main/java/com/ilovesshan/im/config/CurrentUser.java
@@ -0,0 +1,19 @@
+package com.ilovesshan.im.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2023/5/18
+ * @description:
+ */
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
---
+++ src/main/java/com/ilovesshan/im/handler/CurrentUserMethodArgumentResolverHandler.java
@@ -0,0 +1,47 @@
+package com.ilovesshan.im.handler;
+
+import com.ilovesshan.im.config.CurrentUser;
+import com.ilovesshan.im.exception.ImException;
+import com.ilovesshan.im.model.po.User;
+import com.ilovesshan.im.utils.JwtUtil;
+import com.ilovesshan.im.utils.R;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2023/5/18
+ * @description:
+ */
+
+@Configuration
+public class CurrentUserMethodArgumentResolverHandler implements HandlerMethodArgumentResolver {
+
+    @Resource
+    private JwtUtil jwtUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.hasMethodAnnotation(CurrentUser.class) && methodParameter.getParameterType().isAssignableFrom(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        HttpServletRequest httpServletRequest = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        String token = httpServletRequest.getHeader("Authorization");
+        if (StringUtils.isEmpty(token)) {
+            throw new ImException(R.ERROR_INSUFFICIENT_AUTHENTICATION);
+        }
+        return jwtUtil.parseToken(token);
+    }
+}
